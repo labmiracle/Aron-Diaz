09d8fbd4c8209dab30d5a211626988cee089130e
aron0ne <arondiaz.cpn@gmail.com>
2023-04-21 14:55:10 -0300

    Ejercicio 6

---
+++ practica-js/6/app.js
@@ -0,0 +1,25 @@
+//Ejercicio 6
+
+const carrito = {
+    productos: [{
+    nombre: "Papel higienico",
+    unidades: 4,
+    precio: 5
+    },
+    {
+     nombre: "chocolate",
+     unidades: 2,
+     precio: 1.5   
+    }],
+
+    get precioTotal(){
+        let precioTotal = 0;
+        for (let producto of this.productos) {
+          precioTotal += producto.precio * producto.unidades
+        }
+        return precioTotal;
+      }
+
+    }
+
+    console.log(carrito.precioTotal);
