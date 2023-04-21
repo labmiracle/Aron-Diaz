ecf434373a002b8a6bd8409b75d8a6acc36815b8
aron0ne <arondiaz.cpn@gmail.com>
2023-04-20 21:54:49 -0300

    Ejercicio 3 resolucion

---
+++ practica-js/3/app.js
@@ -0,0 +1,18 @@
+//Ejercicio 3
+
+let altura = 0;
+let dias = 0;
+
+function calcularDiasCrecimiento(velocidadCrecimiento, velocidadDecrecimiento, alturaDeseada) {
+    while (altura < alturaDeseada) {
+      altura += velocidadCrecimiento;
+      dias++;
+      if (altura >= alturaDeseada) 
+      break;
+      altura -= velocidadDecrecimiento;
+    }
+    return dias;
+  }
+
+  //177 dias tarda en crecer si la velocidad de crecimiento es 2cm, el decrecimiento es 0.30 y la altura 3 metros
+  console.log(calcularDiasCrecimiento(2,0.30,300));
