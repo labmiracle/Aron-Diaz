70e7d0677c11fa2850b9cf5f96187ab163f58b71
aron0ne <arondiaz.cpn@gmail.com>
2023-04-25 11:10:54 -0300

    Ejercicio 4 y 7

--- practica-js/4/app.js
+++ practica-js/4/app.js
@@ -13,11 +13,22 @@
 
   }
 
-  console.log(palabraMasLarga("Erase una vez que sera"));
+  console.assert(palabraMasLarga("Erase una vez que sera") === "Erase Una Vez Que Sera");
 
 
 
 //Mayuscula 
 
   
+function primeraMayuscula(str){
+  let palabras = str.split(" ");
+  let resultado = "";
+  for (let palabra of palabras){
+    resultado += palabra[0].toUpperCase() + palabra.substr(1) + " ";
+  }
+  return resultado;
+
+}
+
+console.assert(primeraMayuscula("En un lugar de la Mancha") === "En Un Lugar De La Mancha");
 
