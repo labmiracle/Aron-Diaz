50b01bf5a78516d4da817da6e2b61f0458bac0b4
aron0ne <arondiaz.cpn@gmail.com>
2023-06-15 17:48:03 -0300

    Correción del ejercicio js2

--- practica-js/2/app.js
+++ practica-js/2/app.js
@@ -1,13 +1,11 @@
 //Ejercicio 2
 
 function sum(arr, n) {
-  let result = 0;
-  for (let i = 0; i < n; i++) {
-    result += arr[i];
+  if (n === 0) {
+    return 0;
   }
-  return result;
+  return sum(arr, n - 1) + arr[n - 1];
 }
 
-let operation = sum([1, 1, 2, 8, 9, 100], 4);
-
+let operation = sum([1, 7, 3], 2);
 console.log(operation);
